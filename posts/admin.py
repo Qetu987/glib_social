5d904928ab5531db7a27a96bb189fb1656dc72a4
Ruslan <sichkaryk39@gmail.com>
2026-06-18 19:11:46 +0300

    add admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from posts.models import Post
+from posts.models import Post, Tag, Like, Review, Repost, ReviewsLike
 
 
 @admin.register(Post)
@@ -10,3 +10,29 @@ class PostAdmin(admin.ModelAdmin):
     list_editable = ('is_active', 'is_visible')
     ordering = ('-date',)
     list_per_page = 25
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    search_fields = ('title',)
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'post', 'date')
+    search_fields = ('owner__username', 'text')
+    list_filter = ('date',)
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'post', 'date')
+    list_filter = ('date',)
+
+@admin.register(Repost)
+class RepostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'post', 'date')
+    list_filter = ('date',)
+
+@admin.register(ReviewsLike)
+class ReviewsLikeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'review', 'date')
+    list_filter = ('date',)
